afcc86e185d14a4fbe18a798fc7ab6170d9dabbf
codymoose <rcmoose2000@gmail.com>
2017-01-10 20:47:17 -0600

    Added default RobotMap stuff

--- src/edu/jaguarbots/steamworks/RobotMap.java
+++ src/edu/jaguarbots/steamworks/RobotMap.java
@@ -16,4 +16,28 @@ public class RobotMap {
 	// number and the module. For example you with a rangefinder:
 	// public static int rangefinderPort = 1;
 	// public static int rangefinderModule = 1;
+
+  //pwms
+    public static final int leftDrive              = 0;
+    public static final int rightDrive             = 1;
+
+    //relays
+    public static final int relayClimberMotor        = 1;
+    
+    //sol
+
+    // digital ios
+    public static final int leftEncoderAChannel    = 0;
+    public static final int leftEncoderBChannel    = 1;
+    public static final int rightEncoderAChannel   = 2;
+    public static final int rightEncoderBChannel   = 3;
+//    public static final int climberLimitSwitch = 7;
+
+    //spi
+//    public static final int gyro = 0;
+
+    // joysticks
+    public static final int leftStick              = 0;
+    public static final int rightStick             = 1;
+    public static final int manipulator            = 2;
 }
