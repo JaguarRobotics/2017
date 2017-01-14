d1cc8088796e6750d1a2efdcb56ed65e4530cd53
Zach Deibert <zachdeibert@gmail.com>
2017-01-14 14:47:51 -0600

    Actually fixed logging

--- email.sh
+++ email.sh
@@ -13,3 +13,7 @@ cat <<EOF >> email.txt
 Thank you!
 EOF
 sendemail -l email.log -f "$FROM_EMAIL" -u "Lightning McSteam's Code Update" -t "$TO_EMAIL" -s smtp.gmail.com:587 -o tls=yes -xu "$FROM_EMAIL" -xp "$FROM_PASSWORD" -o message-file=email.txt -a code.zip
+status=$?
+echo "Log:"
+cat email.log
+exit $status
