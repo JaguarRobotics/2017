9c94d1d68c0ac636bf57eda6ecc97e8781d73099
Nathan Gawith <NathanGawith@gmail.com>
2017-01-24 19:29:59 -0600

    So aparently I can't use Math.h, and big number stuff. (Part 3)

--- accelerometer/calculations.h
+++ accelerometer/calculations.h
@@ -5,21 +5,6 @@
 
 // Calculation related functions
 typedef long num_t;
-num_t timeSlice;
-num_t xAcceleration;
-num_t yAcceleration;
-num_t xVelocity;
-num_t yVelocity;
-num_t integralOfYAcceleration;
-num_t integralOfYVelocity;
-num_t angle;
-num_t xPosition;
-num_t yPosition;
-num_t setAngle(void);
-num_t addXVelocity(num_t aOutput);
-num_t addYVelocity(num_t aOutput);
-num_t setXPosition(num_t cLength);
-num_t setYPosition(num_t cLength);
 num_t getXPosition(void);
 num_t getYPosition(void);
 
