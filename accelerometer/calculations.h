771a7bf0a6cc24e958a81cb780a745c9eb5f6578
Nathan Gawith <NathanGawith@gmail.com>
2017-01-24 19:26:31 -0600

    So aparently I can't use Math.h, and big number stuff. (Part 2)

--- accelerometer/calculations.h
+++ accelerometer/calculations.h
@@ -1,24 +1,26 @@
 #ifndef FRC1810_CALCULATIONS_H
 #define FRC1810_CALCULATIONS_H
 
+#define MULTIPLICATIVE_CONSTANT 1000
+
 // Calculation related functions
 typedef long num_t;
-float timeSlice;
-float xAcceleration;
-float yAcceleration;
-float xVelocity;
-float yVelocity;
-float integralOfYAcceleration;
-float integralOfYVelocity;
-float angle;
-float xPosition;
-float yPosition;
-void setAngle(void);
-void addXVelocity(float aOutput);
-void addYVelocity(float aOutput);
-void setXPosition(float cLength);
-float setYPosition(float cLength);
-float getXPosition(void);
-float getYPosition(void);
+num_t timeSlice;
+num_t xAcceleration;
+num_t yAcceleration;
+num_t xVelocity;
+num_t yVelocity;
+num_t integralOfYAcceleration;
+num_t integralOfYVelocity;
+num_t angle;
+num_t xPosition;
+num_t yPosition;
+num_t setAngle(void);
+num_t addXVelocity(num_t aOutput);
+num_t addYVelocity(num_t aOutput);
+num_t setXPosition(num_t cLength);
+num_t setYPosition(num_t cLength);
+num_t getXPosition(void);
+num_t getYPosition(void);
 
 #endif
