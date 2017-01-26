b5dc1e9bb49d38ecdb58dfb3cc492b4e1f779849
Zach Deibert <zachdeibert@gmail.com>
2017-01-26 17:20:08 -0600

    Wrote io header

---
+++ accelerometer/io-mock.c
@@ -0,0 +1,8 @@
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include "io.h"
+
+int readAccelerometer(accel_axis_t axis) {
+    printk(KERN_ALERT "Not implemented: readAccelerometer");
+    return 0;
+}
--- accelerometer/io.h
+++ accelerometer/io.h
@@ -1,7 +1,19 @@
 #ifndef FRC1810_IO_H
 #define FRC1810_IO_H
 
+#define ACCEL_AXIS_X 1
+#define ACCEL_AXIS_Y 2
+#define ACCEL_AXIS_Z 3
+typedef int accel_axis_t;
+
 // I/O related functions
 // All of these must be implemented in both `io-mock.c` and `io-real.c`
+/**
+ * Reads an accelerometer's input and returns it
+ *
+ * @param axis The axis to read
+ * @return The voltage output on the accelerometer (in mV)
+ */
+int readAccelerometer(accel_axis_t axis);
 
 #endif
