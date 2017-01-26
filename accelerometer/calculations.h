1f920db80caad7a998316517e2f10a8079215f45
Zach Deibert <zachdeibert@gmail.com>
2017-01-26 17:05:03 -0600

    Fixed some errors in `calculations.c` and `calculations.h`

--- accelerometer/calculations.h
+++ accelerometer/calculations.h
@@ -2,9 +2,9 @@
 #define FRC1810_CALCULATIONS_H
 
 #define MULTIPLICATIVE_CONSTANT 1000
+typedef long num_t;
 
 // Calculation related functions
-typedef long num_t;
 num_t getXPosition(void);
 num_t getYPosition(void);
 
