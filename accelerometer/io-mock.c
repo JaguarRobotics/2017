50d018f49d5508e2b965b4ef1147b854ec665cc3
Zach Deibert <zachdeibert@gmail.com>
2017-02-02 14:37:42 -0600

    Implemented the mock accelerometer

--- accelerometer/io-mock.c
+++ accelerometer/io-mock.c
@@ -3,6 +3,15 @@
 #include "io.h"
 
 int readAccelerometer(accel_axis_t axis) {
-    printk(KERN_ALERT "Not implemented: readAccelerometer");
-    return 0;
+    switch (axis) {
+        case ACCEL_AXIS_X:
+            return 600;
+        case ACCEL_AXIS_Y:
+            return 120;
+        case ACCEL_AXIS_Z:
+            return 300;
+        default:
+            printk(KERN_ALERT "Invalid axis constant: %d.\n", axis);
+            return 0;
+    }
 }
