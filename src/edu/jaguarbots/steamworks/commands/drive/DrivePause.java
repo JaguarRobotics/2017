c0465ab843b8cc26836b2782a62e7882432cde63
Brian Parks <mchosting1062@gmail.com>
2017-02-11 11:28:14 -0600

    Made a Pause command for drive.

---
+++ src/edu/jaguarbots/steamworks/commands/drive/DrivePause.java
@@ -0,0 +1,45 @@
+package edu.jaguarbots.steamworks.commands.drive;
+
+import edu.jaguarbots.steamworks.commands.CommandBase;
+
+/**
+ * Drives the robot in teleop based on left and right joystick inputs.
+ */
+public class DrivePause extends CommandBase {
+	private long waitTime;
+	private long startTime;
+
+	public DrivePause(long waitTime) {
+		requires(driveSubsystem);
+		this.waitTime = waitTime;
+		startTime = System.currentTimeMillis();
+
+	}
+
+	// Called just before this Command runs the first time
+	protected void initialize() {
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		long now = System.currentTimeMillis();
+		if (now - startTime > waitTime) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// Called once after isFinished returns true
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+	}
+}
