6fa39ae9824bbe9cf6e0267fa61144ed589374e6
Brian <Brian@DESKTOP-FKVDET2>
2017-03-24 14:29:40 -0500

    Nathan did not write this. I totally did, dont listen to Nathan or the documentation, or anything anybody but me says

---
+++ src/edu/jaguarbots/steamworks/commands/drive/AutoRecordingDrive.java
@@ -0,0 +1,120 @@
+package edu.jaguarbots.steamworks.commands.drive;
+import java.util.ArrayList;
+import edu.jaguarbots.steamworks.commands.CommandBase;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+/**
+ * Drives the robot in teleop based on left and right joystick inputs.
+ */
+public class AutoRecordingDrive extends CommandBase
+{
+    /**
+     * Below is an image of the idea we are using for joystick vs motor power.
+     * <html><img src="https://puu.sh/tEhvx/a211c4f7a1.png"></img></html>
+     */
+    public AutoRecordingDrive()
+    {
+        requires(driveSubsystem);
+    }
+    double                    left;
+    double                    right;
+    private ArrayList<String> commands     = new ArrayList<String>();
+    private ArrayList<Double> encoderTicks = new ArrayList<Double>();
+    private boolean           isTurning    = false;
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize()
+    {
+        driveSubsystem.resetEncoders(true, true);
+        driveSubsystem.startEncoders();
+    }
+    // Called repeatedly when this Command is scheduled to run
+    /**
+     * Below is an image of the idea we are using for joystick vs motor power.
+     * <html><img src="https://puu.sh/tEhvx/a211c4f7a1.png"></img></html>
+     */
+    @Override
+    @SuppressWarnings("deprecation")
+    protected void execute()
+    {
+        SmartDashboard.putNumber("EncoderLeft",
+                        CommandBase.driveSubsystem.getEncoderLeft());
+        SmartDashboard.putNumber("EncoderRight",
+                        CommandBase.driveSubsystem.getEncoderRight());
+        double powNum = 2;
+        double pointNum = SmartDashboard.getNumber("Joystick Tolerance");
+        /*
+         * // code for Y axis double j0 = oi.Joystick0.getY() * pointNum; double j1 = oi.Joystick1.getY() * pointNum;
+         * double ju = (Math.abs(j0) > Math.abs(j1)) ? j0 : j1; double aju = Math.abs(ju); double pju = Math.pow(aju,
+         * powNum); if (Math.abs(pju) > aju) pju = aju; if (Math.abs(pju) > aju) pju = aju; double straight = (pju *
+         * (aju / ju)) / pointNum; // code for X axis j0 = oi.Joystick0.getX() * pointNum; j1 = oi.Joystick1.getX() *
+         * pointNum; ju = (Math.abs(j0) > Math.abs(j1)) ? j0 : j1; aju = Math.abs(ju); pju = Math.pow(aju, powNum); if
+         * (Math.abs(pju) > aju) pju = aju; if (Math.abs(pju) > aju) pju = aju; double turn = (pju * (aju / ju)) /
+         * pointNum; if (Math.abs(straight) > Math.abs(turn)) driveSubsystem.driveTank(-straight, -straight); else { if
+         * (turn > 0) driveSubsystem.driveTank(turn, -turn); else driveSubsystem.driveTank(-turn, turn); }
+         */
+        // rewrite
+        double j0 = oi.Joystick0.getY() * pointNum;
+        double j1 = oi.Joystick1.getY() * pointNum;
+        double ju1 = (Math.abs(j0) > Math.abs(j1)) ? j0 : j1;
+        j0 = oi.Joystick0.getX() * pointNum;
+        j1 = oi.Joystick1.getX() * pointNum;
+        double ju2 = (Math.abs(j0) > Math.abs(j1)) ? j0 : j1;
+        boolean isTurn = (Math.abs(ju1) > Math.abs(ju2)) ? false : true;
+        double ju = isTurn ? ju2 : ju1;
+        ju = (Math.abs(j0) > Math.abs(j1)) ? j0 : j1;
+        double aju = Math.abs(ju);
+        double pju = Math.pow(aju, powNum);
+        if (Math.abs(pju) > aju) pju = aju;
+        double power = (pju * (aju / ju)) / pointNum;
+        if (isTurn) driveSubsystem.driveTank(-power, -power);
+        else
+        {
+            if (power > 0) driveSubsystem.driveTank(power, -power);
+            else
+                driveSubsystem.driveTank(-power, power);
+        }
+        left = driveSubsystem.getEncoderLeft();
+        right = driveSubsystem.getEncoderRight();
+        if (isTurning == isTurn)
+        {
+            if (isTurn) commands.add("turn");
+            else
+                commands.add("straight");
+            double jrsgn = driveSubsystem.getEncoderRight()
+                            / Math.abs(driveSubsystem.getEncoderRight());
+            double average = (Math.abs(driveSubsystem.getEncoderLeft())
+                            + Math.abs(driveSubsystem.getEncoderRight())) / 2;
+            encoderTicks.add(jrsgn * average);
+            driveSubsystem.resetEncoders(true, true);
+            driveSubsystem.startEncoders();
+        }
+        isTurning = isTurn;
+    }
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished()
+    {
+        return false;
+    }
+    // Called once after isFinished returns true
+    @Override
+    protected void end()
+    {
+        //TODO: need to do the file thing
+        for (int i = 0; i < commands.size(); i++)
+        {
+            if (commands.get(i) == "straight")
+                System.out.println("addSequential(new EncoderDrive("
+                                + encoderTicks.get(i) + ", straightSpeed));");
+            else if (commands.get(i) == "straight")
+                System.out.println("addSequential(new EncoderTurn("
+                                + encoderTicks.get(i) + "));");
+        }
+    }
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted()
+    {
+    }
+}
