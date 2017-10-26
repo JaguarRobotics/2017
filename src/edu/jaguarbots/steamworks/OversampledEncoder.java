36d55b4acd7ab50e2463bf59837625b39925dd08
Zach Deibert <zachdeibert@gmail.com>
2017-10-26 15:27:41 -0500

    Wrote an oversampling algorithm

--- src/edu/jaguarbots/steamworks/OversampledEncoder.java
+++ src/edu/jaguarbots/steamworks/OversampledEncoder.java
@@ -3,13 +3,28 @@ package edu.jaguarbots.steamworks;
 import edu.wpi.first.wpilibj.Encoder;
 
 public class OversampledEncoder extends Encoder {
+    private static final int SAMPLES_TO_AVERAGE = 20;
+    private final double[]   values;
+    private int              frame;
+
     @Override
     public double getDistance() {
-        // TODO oversampling code
-        return super.getDistance();
+        double value = super.getDistance();
+        values[frame % SAMPLES_TO_AVERAGE] = value;
+        if (++frame >= SAMPLES_TO_AVERAGE) {
+            double avg = 0;
+            for (double val : values) {
+                avg += val;
+            }
+            return avg / SAMPLES_TO_AVERAGE;
+        } else {
+            return value;
+        }
     }
 
     public OversampledEncoder(int channelA, int channelB) {
         super(channelA, channelB);
+        values = new double[SAMPLES_TO_AVERAGE];
+        frame = 0;
     }
 }
