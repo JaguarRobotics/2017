95e7b35152cdd7e7eaf703e62fa71d966c7cebec
Zach Deibert <zachdeibert@gmail.com>
2017-10-26 15:18:50 -0500

    Changed encoders to 4x mode

--- src/edu/jaguarbots/steamworks/OversampledEncoder.java
+++ src/edu/jaguarbots/steamworks/OversampledEncoder.java
@@ -3,13 +3,7 @@ package edu.jaguarbots.steamworks;
 import edu.wpi.first.wpilibj.Encoder;
 
 public class OversampledEncoder extends Encoder {
-    @Override
-    public double getDistance() {
-        // TODO oversampling code
-        return super.getDistance();
-    }
-
     public OversampledEncoder(int channelA, int channelB) {
-        super(channelA, channelB);
+        super(channelA, channelB, false, EncodingType.k4X);
     }
 }
